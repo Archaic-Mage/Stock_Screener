e9fe46958596bfa0e0ad680b4644a717ba5c547c
Arunesh28 <arunesh2810@gmail.com>
2023-08-09 02:07:39 +0530

    created data_preprocessing()

--- utils.py
+++ utils.py
@@ -6,6 +6,21 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
 }
 
+def data_preprocessing(df):
+    df_dict=df.to_dict()
+    # Removing \n and trailing spaces
+    for key in df_dict.keys() :
+        new_key = key.replace(" \n", "").rstrip()
+        df_dict[new_key] = df_dict.pop(key)
+    # Removing ',' and converting the string to float
+    remove_hyphen = (lambda x: 0.0 if x == '-' else x)
+    key_list = ['PREV. CLOSE', 'IEP', 'CHNG', '%CHNG', 'FINAL', 'VALUE', 'FFM CAP', 'NM 52W H', 'NM 52W L']
+    for key in key_list:
+        df_dict[key] = {k: float(remove_hyphen(v.replace(",", ""))) for k, v in df_dict[key].items()}
+    df_dict['FINAL QUANTITY'] = {k: int(remove_hyphen(v.replace(",", ""))) for k, v in df_dict['FINAL QUANTITY'].items()}
+    df = pd.DataFrame.from_dict(df_dict)
+    return df
+
 def get_mdata():
     url = 'https://www.nseindia.com/api/equity-stockIndices?csv=true&index=NIFTY%20100'
     #get request
@@ -43,12 +58,10 @@ def screen_mdata():
 def screen_pre_mdata():
     # read the csv file            
     df = pd.read_csv('./data/preopen.csv')
-    
-    df['%CHNG \n'] = df['%CHNG \n'].apply(lambda x: 0.0 if x == '-' else float(x))
-    df['FINAL QUANTITY \n'] = df['FINAL QUANTITY \n'].apply(lambda x: 0 if x == '-' else int(x.replace(',', '')))
+    df = data_preprocessing(df)
     print(df)
     # condition %CHNG should be greater than 1%
-    new_df = df[df['%CHNG \n'].abs() >= 1]
+    new_df = df[df['%CHNG'].abs() >= 1]
     #quantity traded should be more than 50000
-    new_df = new_df[new_df['FINAL QUANTITY \n'] >= 50000]
+    new_df = new_df[new_df['FINAL QUANTITY'] >= 50000]
     print(new_df)

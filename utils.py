071b94fbba9ce06d9e141f56cb878bd891d34664
Arunesh28 <arunesh2810@gmail.com>
2023-08-09 19:55:11 +0530

    data processing issues fixed

--- utils.py
+++ utils.py
@@ -1,24 +1,35 @@
 import requests
 import pandas as pd
 import threading as th
+import sys
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
 }
-
 def data_preprocessing(df):
     df_dict=df.to_dict()
     # Removing \n and trailing spaces
+    new_df_dict = {}
     for key in df_dict.keys() :
-        new_key = key.replace(" \n", "").rstrip()
-        df_dict[new_key] = df_dict.pop(key)
+        new_df_dict[key.replace("\n", "").rstrip()] = df_dict[key]
+    df_dict = new_df_dict
+    
     # Removing ',' and converting the string to float
     remove_hyphen = (lambda x: 0.0 if x == '-' else x)
-    key_list = ['PREV. CLOSE', 'IEP', 'CHNG', '%CHNG', 'FINAL', 'VALUE', 'FFM CAP', 'NM 52W H', 'NM 52W L']
-    for key in key_list:
-        df_dict[key] = {k: float(remove_hyphen(v.replace(",", ""))) for k, v in df_dict[key].items()}
-    df_dict['FINAL QUANTITY'] = {k: int(remove_hyphen(v.replace(",", ""))) for k, v in df_dict['FINAL QUANTITY'].items()}
+    remove_commas = (lambda x: x.replace(",", "") if type(x) == str else x)   
+    for key in df_dict.keys():
+        if key == 'SYMBOL': continue
+        df_dict[key] = {k: float(remove_hyphen(remove_commas(v))) for k, v in df_dict[key].items()}
+        
+    # Certain attributes have to be typecasted to integers 
+    if sys._getframe(1).f_code.co_name == 'get_pre_mdata':
+        df_dict['FINAL QUANTITY'] = {k: int(v) for k, v in df_dict['FINAL QUANTITY'].items()}
+    if sys._getframe(1).f_code.co_name == 'get_mdata':
+        df_dict['VOLUME (shares)'] = {k: int(v) for k, v in df_dict['VOLUME (shares)'].items()}
+    
+    # convert the dictionary back to dataframe
     df = pd.DataFrame.from_dict(df_dict)
+    
     return df
 
 def get_mdata():
@@ -33,6 +44,7 @@ def get_mdata():
     df = pd.read_csv('./data/nifty100.csv')
     for col in df.columns:
         df[col] = df[col].apply(lambda x: x.strip() if type(x) == str else x)
+    df = data_preprocessing(df)
     df.to_csv('./data/nifty100.csv', index=False)
 
 def get_pre_mdata():
@@ -46,19 +58,19 @@ def get_pre_mdata():
     df = pd.read_csv('./data/preopen.csv')
     for col in df.columns:
         df[col] = df[col].apply(lambda x: x.strip() if type(x) == str else x)
+    df = data_preprocessing(df)
     df.to_csv('./data/preopen.csv', index=False)
         
 def screen_mdata():
     # read the csv file
     df = pd.read_csv('./data/nifty100.csv')
     
-    cipla_df = df[df['SYMBOL \n'] == 'CIPLA']
-    print(cipla_df[['SYMBOL \n', 'LTP \n', '%CHNG \n']])
+    cipla_df = df[df['SYMBOL'] == 'CIPLA']
+    print(cipla_df[['SYMBOL', 'LTP', '%CHNG']])
         
 def screen_pre_mdata():
     # read the csv file            
     df = pd.read_csv('./data/preopen.csv')
-    df = data_preprocessing(df)
     print(df)
     # condition %CHNG should be greater than 1%
     new_df = df[df['%CHNG'].abs() >= 1]
